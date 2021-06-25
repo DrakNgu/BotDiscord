5b4d68276c9434addb3f5a640508f9b419f7fe11
DrakNgu <grossetk1@gmail.com>
2021-06-25 10:24:22 +0200

    git init

---
+++ cogs/event.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+import discord
+import yaml
+from discord.ext import commands
+
+if not os.path.isfile("config.yaml"):
+    sys.exit("'config.yaml' not found! Please add it and try again.")
+else:
+    with open("config.yaml") as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+class event(commands.Cog, name="event"):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+
+def setup(bot):
+    bot.add_cog(event(bot))
